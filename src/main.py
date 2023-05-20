0193711596aad32acbc4e1d1742e1b3e40ef1a4b
G00Z_G00Z <edygomezgg4@gmail.com>
2023-05-19 18:05:04 -0600

    updated main to be more simple

--- src/main.py
+++ src/main.py
@@ -1,6 +1,35 @@
-def main():
-    print("hello world!")
+#!/usr/bin/env python
 
+import zmqRemoteApi
+import numpy as np
+from time import sleep
+import pandas as pd
+from utils import rotation_3d_deg
+from utils.simulation import reset_arm
 
-if __name__ == "__main__":
-    main()
+client = zmqRemoteApi.RemoteAPIClient()
+
+# Sim object
+sim = client.getObject("sim")
+
+# Base
+UR5_sim_obj_id = "/UR5"
+base_id = sim.getObject(UR5_sim_obj_id)
+
+# Joint ids
+no_joints = 6
+joint_sim_names = ["" for _ in range(6)]
+
+joint_sim_names[0] = f"{UR5_sim_obj_id}/joint"
+
+for i in range(1, len(joint_sim_names)):
+    link_joint_str = "/link/joint" * i
+    joint_sim_names[i] = f"{UR5_sim_obj_id}{link_joint_str}"
+
+connection_name = f"{UR5_sim_obj_id}/connection"
+
+joint_ids = [sim.getObject(name) for name in joint_sim_names]
+connection_id = sim.getObject(connection_name)
+
+print(joint_ids)
+print(connection_id)
